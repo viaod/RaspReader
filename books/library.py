5b9e939f72048e122829f4cd9ec7e48ee8c37d9f
Vanessa Oduguwa <vio3@kent.ac.uk>
2026-07-08 19:53:36 +0100

    v1.0: Start library functionality

--- books/library.py
+++ books/library.py
@@ -1,1 +1,16 @@
 # Scan books
+
+from pathlib import Path
+import glob
+
+class Library:
+    
+     def __init__(self):
+        
+        books_path = Path(__file__).parent / "/*.epub"
+        
+        print(glob.glob(books_path))
+
+
+if __name__ == "__main__":
+    Library()
