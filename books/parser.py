4be60ad107fd08f52ebda4f6a53371ec86df29fa
Vanessa Oduguwa <vio3@kent.ac.uk>
2026-07-08 19:43:13 +0100

    v1.0: Add printing text to parser testing

--- books/parser.py
+++ books/parser.py
@@ -21,19 +21,19 @@ class Parser:
         print(book.get_metadata("DC", "title"))
         print(book.get_metadata("DC", "creator"))
         
-        # # Extract all HTML documents (chapters) from the book
-        # chapters = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+        # Extract all HTML documents (chapters) from the book
+        chapters = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
 
-        # for chapter in chapters:
-        #     # Get raw HTML content
-        #     content = chapter.get_content()
+        for chapter in chapters:
+            # Get raw HTML content
+            content = chapter.get_content()
 
-        #     # Use BeautifulSoup to strip HTML tags and extract readable text
-        #     soup = BeautifulSoup(content, 'html.parser')
-        #     text = soup.get_text()
+            # Use BeautifulSoup to strip HTML tags and extract readable text
+            soup = BeautifulSoup(content, 'html.parser')
+            text = soup.get_text()
 
-        #     # Print page/chapter text (or send to an e-paper display)
-        #     print(text)
+            # Print page/chapter text (or send to an e-paper display)
+            print(text)
 
 
 if __name__ == "__main__":
