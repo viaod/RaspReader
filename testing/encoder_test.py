1320c2429b759a4de830e0be6a7d35c338189573
Vanessa Oduguwa <vio3@kent.ac.uk>
2026-07-09 21:10:15 +0100

    v1.0: Fix encoder test

--- testing/encoder_test.py
+++ testing/encoder_test.py
@@ -1,7 +1,12 @@
+import os
+import sys
 import time
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
 
 from encoder import Encoder
-
 
 encoder = Encoder()
 encoder.initialise()
