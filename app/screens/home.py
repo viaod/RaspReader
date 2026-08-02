965166f6f43d3ed26bc782da149c099f9278e5e4
Vanessa Oduguwa <vio3@kent.ac.uk>
2026-08-02 22:14:20 +0100

    v0.1.1: fix home screen init

--- app/screens/home.py
+++ app/screens/home.py
@@ -1,12 +1,13 @@
 from app.core.events import Event
+from app.reader import book_reader
 from app.screen import Screen
 from app.screens.main_menu import MainMenu
 
 
 class HomeScreen(Screen):
 
-    def __init__(self, display, assets_dir=None, ui=None, **kwargs):
-        super().__init__(display, assets_dir, ui, **kwargs)
+    def __init__(self, display, assets_dir=None, ui=None, book_reader=None):
+        super().__init__(display, assets_dir, ui, book_reader)
 
     def show(self):
         image_path = self.assets_dir / "images" / "raspreader_home.bmp"
